d1241d1e3356a414626f122303661fbcf2bceca2
Nick Owen <nowen@scottlogic.com>
2015-10-27 13:37:06 +0000

    Incorporate boundaries

--- map.js
+++ map.js
@@ -6,19 +6,24 @@
         .attr('width', width)
         .attr('height', height);
 
+    var projection = d3.geo.orthographic()
+        .translate([width / 2, height / 2])
+        .scale(400)
+        .clipAngle(90)
+        .rotate([0, 0, -23.4]); // yaw, pitch, roll
+
     d3.json('world.json', function(error, world) {
         if (error) {
             return console.error(error);
         }
 
         var subunits = topojson.feature(world, world.objects.subunits),
-            projection = d3.geo.orthographic()
-                .scale(300)
-                .translate([width / 2, height / 2])
-                .rotate([0, 0, -23.4]); // yaw, pitch, roll
+            path = d3.geo.path().projection(projection);
 
-        svg.append('path')
-            .datum(subunits)
-            .attr('d', d3.geo.path().projection(projection));
+        svg.selectAll('.subunit')
+            .data(topojson.feature(world, world.objects.subunits).features)
+            .enter().append('path')
+            .attr('class', function(d) { return 'subunit ' + d.id; })
+            .attr('d', path);
     })
 }());
