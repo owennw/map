cc3b37510f5f54b72d8b152da7801b1786910472
Nick Owen <nowen@scottlogic.com>
2015-10-27 12:24:33 +0000

    Create simple globe

---
+++ map.js
@@ -0,0 +1,23 @@
+﻿(function() {
+    var width = 1600,
+        height = 900;
+
+    var svg = d3.select('body').append('svg')
+        .attr('width', width)
+        .attr('height', height);
+
+    d3.json('world.json', function(error, world) {
+        if (error) {
+            return console.error(error);
+        }
+        
+        var subunits = topojson.feature(world, world.objects.subunits),
+            projection = d3.geo.orthographic()
+                .scale(300)
+                .translate([width / 2, height / 2]);
+
+        svg.append('path')
+            .datum(subunits)
+            .attr('d', d3.geo.path().projection(projection));
+    })
+}());
